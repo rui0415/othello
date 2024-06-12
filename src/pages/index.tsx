32783bb4f3e2a1dd107a628f30f0c91fe6e4dee3
rui0415 <rui20030415@gmail.com>
2024-06-13 03:29:15 +0900

    リファクタリング

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,10 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  const [turnColor, setTurnColor] = useState(1);
-  const [turnCount, setTurnCount] = useState(1);
-  const [black, setBlack] = useState(2);
-  const [white, setWhite] = useState(2);
-  const [pass, setPass] = useState(false);
-  const [endGame, setEndGame] = useState(false);
+  const [turn, setTurn] = useState({ color: 1, count: 1 });
+  const [colorCount, setColorCount] = useState({ black: 2, white: 2 });
+
+  const [pass, setPass] = useState({ pass: false, endGame: false });
   let blackCount;
   let whiteCount;
 
@@ -22,9 +20,13 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
-  const [prev, setPrev] = useState(board);
-  const [prevColor, setPrevColor] = useState(1);
-  const [prevTurnCount, setPrevTurnCount] = useState(1);
+  const [prevTurn, setPrevTurn] = useState({
+    prevBoard: board,
+    prevColor: 1,
+    prevCount: 1,
+    prevBlackCount: 2,
+    prevWhiteCount: 2,
+  });
 
   const directions = [
     [0, 1],
@@ -38,9 +40,9 @@ const Home = () => {
   ];
 
   const prevBoard = () => {
-    setBoard(prev);
-    setTurnColor(prevColor);
-    setTurnCount(prevTurnCount);
+    setBoard(prevTurn.prevBoard);
+    setTurn({ color: prevTurn.prevColor, count: prevTurn.prevCount });
+    setColorCount({ black: prevTurn.prevBlackCount, white: prevTurn.prevWhiteCount });
   };
 
   const boardRange = (x: number, y: number) => {
@@ -51,17 +53,19 @@ const Home = () => {
     let flag = 0;
 
     if (x === -2 && y === -2) {
-      //パスしたときの例外処理
-      setTurnColor(2 / turnColor);
+      //パスしたときの処理
+      setTurn({ color: 2 / turn.color, count: turn.count });
       flag = 1;
-    } else if (x === -3 && y === -3) {
-      flag = 1;
-    } else if (board[y][x] !== 0 && board[y][x] !== -1) return;
+    } else if (board[y][x] === 1 || board[y][x] === 2) return; // 黒か白の石が置かれていればreturn
     const newBoard = structuredClone(board);
     const prevBoard = structuredClone(board);
-    setPrev(prevBoard);
-    setPrevColor(turnColor);
-    setPrevTurnCount(turnCount);
+    setPrevTurn({
+      prevBoard,
+      prevColor: turn.color,
+      prevCount: turn.count,
+      prevBlackCount: colorCount.black,
+      prevWhiteCount: colorCount.white,
+    });
     blackCount = 0;
     whiteCount = 0;
 
@@ -69,16 +73,16 @@ const Home = () => {
       for (const dir of directions) {
         let y_a = y + dir[0];
         let x_a = x + dir[1];
-        if (boardRange(x_a, y_a) && newBoard[y_a][x_a] === 2 / turnColor) {
+        if (boardRange(x_a, y_a) && newBoard[y_a][x_a] === 2 / turn.color) {
           //その方向に違う色が来たら
 
           for (; boardRange(x_a, y_a); y_a += dir[0], x_a += dir[1]) {
-            if (boardRange(x_a, y_a) && newBoard[y_a][x_a] === turnColor) {
+            if (boardRange(x_a, y_a) && newBoard[y_a][x_a] === turn.color) {
               // 違う色が来た列を見続けて、同じ色が来たら
               for (; y_a !== y || x_a !== x; y_a -= dir[0], x_a -= dir[1]) {
-                newBoard[y_a][x_a] = turnColor; //裏返し
+                newBoard[y_a][x_a] = turn.color; //裏返し
               }
-              newBoard[y][x] = turnColor;
+              newBoard[y][x] = turn.color;
 
               flag = 1;
               break;
@@ -100,19 +104,16 @@ const Home = () => {
 
       for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
-          if (newBoard[i][j] === 2 / turnColor) {
+          if (newBoard[i][j] === 2 / turn.color) {
             // 盤面をすべて見て、相手の色が置かれたマスだったら
             for (const dir of directions) {
               let y_a = i + dir[0];
               let x_a = j + dir[1];
-              if (boardRange(x_a, y_a) && newBoard[y_a][x_a] === turnColor) {
+              if (boardRange(x_a, y_a) && newBoard[y_a][x_a] === turn.color) {
                 // 全方向みて、それが自分の色だったら
                 for (; boardRange(x_a, y_a); y_a += dir[0], x_a += dir[1]) {
-                  if (newBoard[y_a][x_a] === 2 / turnColor) break;
-                  if (
-                    boardRange(x_a, y_a) &&
-                    (newBoard[y_a][x_a] === 0 || newBoard[y_a][x_a] === -1)
-                  ) {
+                  if (newBoard[y_a][x_a] === 2 / turn.color) break;
+                  if (boardRange(x_a, y_a) && newBoard[y_a][x_a] <= 0) {
                     // 違う色が来た列を見続けて、空白が来たら
                     newBoard[y_a][x_a] = -1;
                     break;
@@ -123,8 +124,7 @@ const Home = () => {
           }
         }
       }
-      setTurnColor(2 / turnColor);
-      setTurnCount(turnCount + 1);
+      setTurn({ color: 2 / turn.color, count: turn.count + 1 });
     } else {
       return; //石が置けていないならリターンする
     }
@@ -133,12 +133,12 @@ const Home = () => {
     const board_flat = newBoard.flat();
     const board_candidate = board_flat.filter((color) => color === -1);
     const board_space = board_flat.filter((color) => color <= 0);
-    if ((pass && board_candidate.length === 0) || board_space.length === 0) {
-      setEndGame(true);
+    if ((pass.pass && board_candidate.length === 0) || board_space.length === 0) {
+      setPass({ pass: true, endGame: true });
+    } else {
+      setPass({ pass: board_candidate.length ? false : true, endGame: false });
     }
-    setPass(board_candidate.length ? false : true);
-    setBlack(blackCount);
-    setWhite(whiteCount);
+    setColorCount({ black: blackCount, white: whiteCount });
   };
 
   return (
@@ -147,14 +147,14 @@ const Home = () => {
         ひとつ前に戻る
       </div>
       <div className={styles.header}>
-        <div className={styles.whiteCount}>{white}</div>
+        <div className={styles.whiteCount}>{colorCount.white}</div>
 
         <div className={styles.turn}>
-          <div>{turnColor === 1 ? '黒' : '白'}のターン</div>
-          <div>{turnCount}ターン目</div>
+          <div>{turn.color === 1 ? '黒' : '白'}のターン</div>
+          <div>{turn.count}ターン目</div>
         </div>
 
-        <div className={styles.blackCount}>{black}</div>
+        <div className={styles.blackCount}>{colorCount.black}</div>
       </div>
       <div className={styles.boardstyle}>
         {board.map((row, y) =>
@@ -175,12 +175,16 @@ const Home = () => {
           )),
         )}
       </div>
-      {pass && !endGame && (
+      {pass.pass && !pass.endGame && (
         <div className={styles.pass} onClick={() => clickHandler(-2, -2)}>
           パス
         </div>
       )}
-      {endGame && <div className={styles.endgame}>{black > white ? 'Black Win' : 'White Win'}</div>}
+      {pass.endGame && (
+        <div className={styles.endgame}>
+          {colorCount.black > colorCount.white ? 'Black' : 'White'} Win
+        </div>
+      )}
     </div>
   );
 };
